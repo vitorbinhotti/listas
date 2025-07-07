7ee4345c3206b7e032c3ad5369eea555ecdedab3
vitorbinhotti <vitor_binhotti@estudante.sesisenai.org.br>
2025-07-07 12:04:58 -0300

    update - exercicio 8

---
+++ lista1/exercicio8.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contador número pares</title>
+</head>
+<body>
+    <h1>Contador número pares</h1>
+    <h2>Não consegui realizar.</h2>
+</body>
+</html>
