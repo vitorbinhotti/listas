a7c57794b241cf0e21852b9c1b7c522bfae5eb73
vitorbinhotti <vitor_binhotti@estudante.sesisenai.org.br>
2025-07-09 08:57:47 -0300

    update - exercicio 8

--- lista1/exercicio8.php
+++ lista1/exercicio8.php
@@ -7,6 +7,25 @@
 </head>
 <body>
     <h1>Contador número pares</h1>
-    <h2>Não consegui realizar.</h2>
+
+    <form method="POST" action="">
+        <label for="numero">Digite um número para descobrir os números pares entre o número 1 e o número inserido: </label>
+        <input type="number" id="numero" name="numero" required>
+        <button type="submit" name="verificar_numero">Verificar</button>
+    </form>
+
 </body>
 </html>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['verificar_numero'])) {
+        $numero = $_POST['numero'];
+    };
+    if (($numero) && ($numero > 0)) {
+        for ($i = 2; $i <= $numero; $i += 2) {
+            echo $i . " ";
+        };
+    };
+};
+?>
