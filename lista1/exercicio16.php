deac15a23371f6e183bbf8582604cd3405c02c44
vitorbinhotti <vitor_binhotti@estudante.sesisenai.org.br>
2025-07-09 09:08:51 -0300

    update - exercicio 16

---
+++ lista1/exercicio16.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pode Votar ou Não</title>
+</head>
+<body>
+    <h1>Descobrir se pode votar ou não</h1>
+
+    <form method="POST" action="">
+        <label for="nome">Digite seu nome: </label>
+        <input type="text" id="nome" name="nome" required>
+        <br><br>
+        <label for="idade">Digite sua idade: </label>
+        <input type="number" id="idade" name="idade" required>
+        <br><br>
+        <button type="submit" name="verificar_numero">Verificar</button>
+    </form>
+
+</body>
+</html>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['verificar_numero'])) {
+        $nome = $_POST['nome'];
+        $idade = $_POST['idade'];
+    };
+    if ($idade >= 1) {
+        echo 'Você pode votar';
+    } else if ($idade < 0) {
+        echo "Idade inválida";
+    } else {
+        echo "Você não pode votar";
+    } 
+};
+?>
